85bdf3840f940fb72d9034a91d58eef374326ad9
0xPlish <plish@hylo.so>
2024-10-30 16:23:12 -0700

    fix flake

--- flake.nix
+++ flake.nix
@@ -1,16 +1,22 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/22.04";
+    nixpkgs.url = "github:NixOS/nixpkgs/24.05";
+    rust-overlay.url =
+      "github:oxalica/rust-overlay/7df2ac544c203d21b63aac23bfaec7f9b919a733";
   };
-
-  outputs = { nixpkgs }:
-      with nixpkgs; {
-        devShell = mkShell {
-          buildInputs=[
-            nodePackages.pnpm
-            nodePackages.typescript
-            nodePackages.typescript-language-server
-          ];
-        };
+  outputs = { self, nixpkgs, rust-overlay }:
+    with import nixpkgs {
+      system = "aarch64-darwin";
+      overlays = [ (import rust-overlay) ];
+    }; {
+      devShell.aarch64-darwin = mkShell {
+        buildInputs = [
+          rust-bin.stable.latest.default
+          rust-analyzer
+          nodePackages.pnpm
+          nodePackages.typescript
+          nodePackages.typescript-language-server
+        ];
       };
+    };
 }
